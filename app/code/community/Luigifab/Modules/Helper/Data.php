0cc3646afb162c125dfbcbcba46b8da24d07d0cd
Fabrice Creuzot <code@luigifab.fr>
2012-07-29 20:00:00 +0000

    Version 1.0.0

---
+++ app/code/community/Luigifab/Modules/Helper/Data.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created V/20/07/2012
+ * Updated S/21/07/2012
+ * Version 3
+ *
+ * Copyright 2012 | Fabrice Creuzot (luigifab) <code~luigifab~info>
+ * https://redmine.luigifab.info/projects/magento/wiki/modules
+ *
+ * This program is free software, you can redistribute it or modify
+ * it under the terms of the GNU General Public License (GPL) as published
+ * by the free software foundation, either version 2 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty, without even the implied warranty of
+ * merchantability or fitness for a particular purpose. See the
+ * GNU General Public License (GPL) for more details.
+ */
+
+class Luigifab_Modules_Helper_Data extends Mage_Core_Helper_Abstract {
+
+	public function getVersion() {
+		return (string) Mage::getConfig()->getModuleConfig('Luigifab_Modules')->version;
+	}
+
+	public function getModulesList($core = false) {
+
+		$data = array();
+
+		foreach (Mage::getConfig()->getNode('modules')->children() as $item) {
+
+			$data[(string) $item->codePool][] = array(
+				'version' => (string) $item->version,
+				'update' => (string) $item->update,
+				'name' => $item->getName()
+			);
+		}
+
+		if (!$core)
+			unset($data['core']);
+
+		return $data;
+	}
+
+	public function checkModuleVersion($name, $url) {
+
+		if (strlen($url) < 5)
+			return false;
+
+		try {
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			$response = curl_exec($curl);
+			curl_close($curl);
+
+			if (strpos($response, '<modules>') !== false) {
+
+				$xml = new DomDocument();
+				$xml->loadXML($response);
+
+				foreach ($xml->getElementsByTagName(strtolower($name)) as $module) {
+
+					return array(
+						'version' => $module->getElementsByTagName('version')->item(0)->firstChild->nodeValue,
+						'url' => $module->getElementsByTagName('url')->item(0)->firstChild->nodeValue
+					);
+				}
+
+				return array();
+			}
+		}
+		catch (Exception $e) {
+			Mage::log($e->getMessage().' for '.$url.' ('.$name.')', Zend_Log::ERR);
+		}
+
+		return false;
+	}
+}
